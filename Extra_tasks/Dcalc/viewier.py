501cfe30325e07bc3cf077be0ac3f52d46a23447
German121 <sgerman2005@gmail.com>
2023-10-13 15:12:16 +0700

    extra3

--- Extra_tasks/Dcalc/viewier.py
+++ Extra_tasks/Dcalc/viewier.py
@@ -2,12 +2,12 @@ import dcalc
 
 
 def create_list(*args, **kwargs):
-    arr = []
-    for i in range(len(args)):
-        arr.append(f'Point_{i} = {dcalc.deg_to_gms(args[i])}')
-    for i, j in kwargs.items():
-        arr.append(f'{i} = {dcalc.deg_to_gms(j)}')
-    return arr
+    a = []
+    for q in range(len(args)):
+        a.append(f'Point_{q} = {dcalc.deg_to_gms(args[q])}')
+    for q, p in kwargs.items():
+        a.append(f'{q} = {dcalc.deg_to_gms(p)}')
+    return a
 
 
 print(create_list(172.25899161, 321.42304971, 12.697987681352, pole=21.89617856, put_1=140.85706448))
